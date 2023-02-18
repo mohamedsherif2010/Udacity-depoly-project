761711df4005313cf5a16d3ae38de1a474d9ef2f
Mohamed Sherif <39101726+mohamedsherif2010@users.noreply.github.com>
2023-02-18 08:06:48 +0200

    Intial Commit

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -3,10 +3,10 @@ import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
   username: config.username,
-  password: config.password,
+  password: "postgres",
   database: config.database,
-  host: config.host,
-
+  host: "database-2.cpbmzmsxsvx2.us-east-1.rds.amazonaws.com",
+  port: 5432,
   dialect: "postgres",
   storage: ":memory:",
 });
